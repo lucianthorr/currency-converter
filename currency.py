95e5d56d8a77046a70b8b78fc7ab9f7d792393f4
lucianthorr <jason.aylward@gmail.com>
2015-01-20 14:18:23 -0500

    same_rate() works.

--- currency.py
+++ currency.py
@@ -5,6 +5,7 @@ No functions longer than 7 lines of code
 """
 
 def get_rate(rates, original_currency, to):
+    rate = 0
     if [rate for origin, dest, rate in rates
         if (origin == original_currency and dest == to)][0]:
             return rate
@@ -15,10 +16,16 @@ def get_rate(rates, original_currency, to):
         return 0
 
 
+
 def same_rate(original_currency, to):
-    pass
+    if original_currency == to:
+        return True
+    else:
+        return False
 
 def convert(rates, value, original_currency, to):
+    if same_rate(original_currency, to):
+        return value
     return value * get_rate(rates, original_currency, to)
 
 if __name__ == '__main__':
