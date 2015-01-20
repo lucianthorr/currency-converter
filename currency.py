6a9fffa167b914ef762871ef1cb675626c7376f1
lucianthorr <jason.aylward@gmail.com>
2015-01-20 14:14:08 -0500

    Separate same currency into own function.

--- currency.py
+++ currency.py
@@ -5,12 +5,21 @@ No functions longer than 7 lines of code
 """
 
 def get_rate(rates, original_currency, to):
+    if [rate for origin, dest, rate in rates
+        if (origin == original_currency and dest == to)][0]:
+            return rate
+    elif [rate for origin, dest, rate in rates
+        if (origin == to and dest == original_currency)][0]:
+            return rate
+    else:
+        return 0
+
+
+def same_rate(original_currency, to):
     pass
 
 def convert(rates, value, original_currency, to):
-    if original_currency == to:
-        return value
-    #return value * get_rate(rates, original_currency, to)
+    return value * get_rate(rates, original_currency, to)
 
 if __name__ == '__main__':
     rates = [("USD", "EUR", 0.866)
