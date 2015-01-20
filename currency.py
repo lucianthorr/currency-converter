318258861ef81b2a17ff1c1ddec6201550e34513
lucianthorr <jason.aylward@gmail.com>
2015-01-20 14:31:02 -0500

    Added Larger Value Tests and realized I needed to round() convert's return.

--- currency.py
+++ currency.py
@@ -5,15 +5,14 @@ No functions longer than 7 lines of code
 """
 
 def get_rate(rates, original_currency, to):
-    rate = 0
-    if [rate for origin, dest, rate in rates
-        if (origin == original_currency and dest == to)][0]:
-            return rate
-    elif [rate for origin, dest, rate in rates
-        if (origin == to and dest == original_currency)][0]:
-            return rate
-    else:
+    rate = [rate for origin, dest, rate in rates
+            if (origin == original_currency and dest == to)][0]
+    if not rate:
+        [rate for origin, dest, rate in rates
+            if (origin == to and dest == original_currency)][0]
+    elif not rate:
         return 0
+    return rate
 
 
 
@@ -26,7 +25,7 @@ def same_rate(original_currency, to):
 def convert(rates, value, original_currency, to):
     if same_rate(original_currency, to):
         return value
-    return value * get_rate(rates, original_currency, to)
+    return round(value * get_rate(rates, original_currency, to),3)
 
 if __name__ == '__main__':
     rates = [("USD", "EUR", 0.866)
--- test_currency.py
+++ test_currency.py
@@ -9,13 +9,13 @@ def test_convert_same_currency():
     assert convert(rates,12,"USD","USD") == 12
     assert convert(rates,0.12,"EUR","EUR") == 0.12
 
-#def test_easy_convert():
-#    assert convert(rates,1,"USD","EUR") == 0.866
-#    assert convert(rates,1,"EUR","JPY") == 0.007
+def test_easy_convert():
+    assert convert(rates,1,"USD","EUR") == 0.866
+    assert convert(rates,1,"EUR","JPY") == 0.007
 
-#def test_larger_convert():
-    #assert convert(rates,50,"USD","EUR") == 43.3
-    #assert convert(rates,25,"EUR","JPY") == 0.175
+def test_larger_convert():
+    assert convert(rates,50,"USD","EUR") == 43.3
+    assert convert(rates,25,"EUR","JPY") == 0.175
 
 def test_get_same_rate():
     assert same_rate("USD", "USD") == True
