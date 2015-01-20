4c3034554ba336946f0fffd4a2d5966dac38d78c
lucianthorr <jason.aylward@gmail.com>
2015-01-20 13:42:25 -0500

    Easy Convert Test Fails.

--- test_currency.py
+++ test_currency.py
@@ -1,5 +1,14 @@
 from currency import *
 
-def test_convert_parameters():
-        assert convert([],12,"USD","USD") == 12
-        assert convert([],0.12,"EUR","EUR") == 0.12
+rates = [("USD", "EUR", 0.866),
+         ("EUR", "USD", 1.154),
+         ("EUR","JPY", 0.007),
+         ("JPY","EUR", 137.20)]
+
+def test_convert_same_currency():
+    assert convert(rates,12,"USD","USD") == 12
+    assert convert(rates,0.12,"EUR","EUR") == 0.12
+
+def test_convert_easy_convert():
+    assert convert(rates,1,"USD","EUR") == 0.866
+    assert convert(rates,1,"EUR","JPY") == 0.007
