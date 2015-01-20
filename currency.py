ab2a2ade8112cf4ba033c083a7004b427a02bc0e
lucianthorr <jason.aylward@gmail.com>
2015-01-20 15:00:41 -0500

    Cleaned up for PEP8

--- currency.py
+++ currency.py
@@ -1,18 +1,15 @@
-"""
-Test!
+# Homework 5
+# Jason Aylward
 
-No functions longer than 7 lines of code
-"""
 
 def get_rate(rates, original_currency, to):
     try:
         rate = [rate for origin, dest, rate in rates
-            if (origin == original_currency and dest == to)][0]
+                if (origin == original_currency and dest == to)][0]
         return rate
     except IndexError:
         print("Unsupported Currency")
         return 0
-
 
 
 def same_rate(original_currency, to):
@@ -21,14 +18,15 @@ def same_rate(original_currency, to):
     else:
         return False
 
+
 def convert(rates, value, original_currency, to):
     if same_rate(original_currency, to):
         return value
-    return round(value * get_rate(rates, original_currency, to),3)
+    return round(value * get_rate(rates, original_currency, to), 3)
 
 if __name__ == '__main__':
     rates = [("USD", "EUR", 0.866)
              ("EUR", "USD", 1.154)
-             ("EUR","JPY", 0.007)
-             ("JPY","EUR", 137.20)]
-    convert(rates,1,"USD","EUR")
+             ("EUR", "JPY", 0.007)
+             ("JPY", "EUR", 137.20)]
+    convert(rates, 1, "USD", "EUR")
--- test_currency.py
+++ test_currency.py
@@ -1,27 +1,38 @@
 from currency import *
 
+
 rates = [("USD", "EUR", 0.866),
          ("EUR", "USD", 1.154),
-         ("EUR","JPY", 0.007),
-         ("JPY","EUR", 137.20)]
+         ("EUR", "JPY", 0.007),
+         ("JPY", "EUR", 137.20)]
+
 
 def test_convert_same_currency():
-    assert convert(rates,12,"USD","USD") == 12
-    assert convert(rates,0.12,"EUR","EUR") == 0.12
+    assert convert(rates, 12, "USD", "USD") == 12
+    assert convert(rates, 0.12, "EUR", "EUR") == 0.12
+
 
 def test_easy_convert():
-    assert convert(rates,1,"USD","EUR") == 0.866
-    assert convert(rates,1,"EUR","JPY") == 0.007
+    assert convert(rates, 1, "USD", "EUR") == 0.866
+    assert convert(rates, 1, "EUR", "JPY") == 0.007
+
 
 def test_euro_to_dollar():
-    assert convert(rates,1,"EUR","USD") == 1.154
+    assert convert(rates, 1, "EUR", "USD") == 1.154
+
 
 def test_larger_convert():
-    assert convert(rates,50,"USD","EUR") == 43.3
-    assert convert(rates,25,"EUR","JPY") == 0.175
+    assert convert(rates, 50, "USD", "EUR") == 43.3
+    assert convert(rates, 25, "EUR", "JPY") == 0.175
+
+
+def test_larger_backward():
+    assert convert(rates, 50, "EUR", "USD")
+
 
 def test_get_same_rate():
-    assert same_rate("USD", "USD") == True
+    assert same_rate("USD", "USD") is True
+
 
 def test_bad_rate():
-    assert convert(rates,1,"CZK","DNM") == 0
+    assert convert(rates, 1, "CZK", "DNM") == 0
